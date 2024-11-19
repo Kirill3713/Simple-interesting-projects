2af0b9e72068ef536e8d35880d4dffae04a09bf8
Kirill <kiryap10@gmail.com>
2024-11-19 13:57:29 +0200

    Adding comments

--- Високосный год.py
+++ Високосный год.py
@@ -6,6 +6,7 @@ cyan = colorama.Fore.CYAN
 light_white = colorama.Fore.LIGHTWHITE_EX
 blue = colorama.Fore.BLUE
 light_blue = colorama.Fore.LIGHTBLUE_EX
+# Создаем функцию
 def check_leap_year(year:int|str) -> bool:
     """
     Функция для проверки високосности года
